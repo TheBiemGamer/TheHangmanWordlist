e845fdd94590f340bdf411818585703767e390db
Jurriaaaantje <jmpjurriaanmichaportier@gmail.com>
2024-09-30 16:54:38 +0200

    Update local wordlist from GIT

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import requests
 import json
 import random
+import os
 
-wordlist = "https://raw.githubusercontent.com/TheBiemGamer/TheHangmanWordlist/refs/heads/main/wordlist.json"
-response = requests.get(wordlist)
+def pull_wordlist():
+    json_file = requests.get(
+        "https://raw.githubusercontent.com/TheBiemGamer/TheHangmanWordlist/refs/heads/main/wordlist.json")
+    wordlist = json_file.json()
+    with open("wordlist.json", 'w') as input_file:
+        input_file.seek(0)
+        json.dump(wordlist, input_file, indent=4)
+    return json.loads(json_file.text)
+if not os.path.exists("wordlist.json"):
+    wordlist = pull_wordlist()
+else:
+    local_json_file = open("wordlist.json", "r").read()
+    local_wordlist = json.loads(local_json_file)
+    local_version = float(local_wordlist["version"])
+    online_json_file = requests.get("https://raw.githubusercontent.com/TheBiemGamer/TheHangmanWordlist/refs/heads/main/wordlist.json")
+    online_wordlist = json.loads(online_json_file.text)
+    online_version = float(local_wordlist["version"])
+    if online_version > local_version:
+        wordlist = pull_wordlist()
+    else:
+        wordlist = local_wordlist
 
-data = json.loads(response.text)
 difficulty = input("What difficulty do you want? (easy/medium/hard): ")
-word = random.choice(data[difficulty])
+word = random.choice(wordlist[difficulty])
 print(word)
+
